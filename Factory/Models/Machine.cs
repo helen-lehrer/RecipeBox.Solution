a54e519a37aa7b91eec4732b1841b038891abd91
Helen Lehrer <hlehrer@outlook.com>
2022-10-14 08:49:30 -0700

    Add Machine Model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,19 +1,20 @@
-// using System;
-// using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-// namespace Factory.Models
-// {
-//   public class Patient
-//   {
-//     public Patient() 
-//     {
-//       this.JoinEntities = new HashSet<PatientDoctor>();
-//     }
-//     public int PatientId { get; set; }
-//     public string PatientName { get; set; }
-//     [DataType(DataType.Date)]
-//     public DateTime AppointmentDate { get; set; }
-//     public virtual ICollection<PatientDoctor> JoinEntities { get; } 
-//   }
-// }
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine() 
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+    public int MachineId { get; set; }
+    public string MachineName { get; set; }
+    public string MachineStatus { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime InstallationDate { get; set; }
+    public virtual ICollection<MachineDoctor> JoinEntities { get; } 
+  }
+}
