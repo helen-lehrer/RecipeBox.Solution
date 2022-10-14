9cf327a0bd7dc6c1d68241a7aa59f2f05fa102f8
Helen Lehrer <hlehrer@outlook.com>
2022-10-14 09:06:34 -0700

    Add FactoryContext for database

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -1,19 +1,19 @@
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-// namespace Factory.Models
-// {
-//   public class FactoryContext : DbContext
-//   {
-//     public DbSet<Patient> Patients { get; set; }
-//     public DbSet<Doctor> Doctors { get; set; }
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public DbSet<Machine> Machines { get; set; }
+    public DbSet<Engineer> Engineers { get; set; }
 
-//     public DbSet<PatientDoctor> PatientDoctor { get; set; }
+    public DbSet<EngineerMachine>EngineerMachine { get; set; }
 
-//     public FactoryContext(DbContextOptions options) : base(options) { }
+    public FactoryContext(DbContextOptions options) : base(options) { }
 
-//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//     {
-//       optionsBuilder.UseLazyLoadingProxies();
-//     }
-//   }
-// }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
