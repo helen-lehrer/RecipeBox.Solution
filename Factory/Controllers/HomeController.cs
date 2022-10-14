bf2f573cc049f771b929efa0ca70da1a49961cd2
Helen Lehrer <hlehrer@outlook.com>
2022-10-14 09:26:21 -0700

    Add Home Controller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
-// namespace Factory.Controllers
-// {
-//     public class HomeController : Controller
-//     {
-//       [HttpGet("/")]
-//       public ActionResult Index()
-//       {
-//         return View();
-//       }
-//     }
-// }
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+    }
+}
